7cc257b21d3d8f2eebf0ba6d1d9f28bf820410e8
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-03-03 22:02:47 +0530

    add the context api for user logged in or not usecontext

---
+++ frontend/src/context/Authcontext.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createContext } from 'react'
+import { useState } from 'react'
+import { isUserLoggedIn } from '../api/auth.user'
+import useFetchData from '../hooks/useFetchData'
+import { useEffect } from 'react'
+import { useContext } from 'react'
+
+ const authContext=createContext() 
+export default function AuthProvider({children}) {
+  const {data, error, loading, execute } = useFetchData(isUserLoggedIn)
+  useEffect(() => {
+  ;(async()=>execute())() 
+  }, []);
+  return (
+    <authContext.Provider value={{data: data?.message, error, loading}} >
+      {children}
+    </authContext.Provider>
+  )
+}
+
+export const useAuth=()=>{
+  const context = useContext(authContext);
+  if (!context) {
+    throw new Error("Error on AuthContext");
+  }
+  
+  return context;
+}
