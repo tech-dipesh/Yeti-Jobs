affc7455469e3b6b958d87e2fa9fbca8cff11ee7
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-03-23 22:41:06 +0530

    not just make all the loading page also make the eager page some page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,39 +7,38 @@ import { RouterProvider, createBrowserRouter } from "react-router"
 const Alluser = lazy(() => import('./pages/Admin/Alluser'))
 const Individualuser = lazy(() => import('./pages/User/Profile'))
 const Edituser = lazy(() => import('./pages/User/Edituser'))
-const ProfilePhoto = lazy(() => import('./pages/User/addProfilePhoto'))
 const Addresume = lazy(() => import('./pages/User/AddResume'))
 
 const GetallApplied = lazy(() => import('./pages/Applications/GetallApplied'))
 const Jobapplicant = lazy(() => import('./pages/Applications/JobApplicant'))
-const AuthProvider = lazy(() => import('./context/Authcontext'))
 const Allcompanies = lazy(() => import('./pages/Companies/Allcompanies'))
 const NewCompany = lazy(() => import('./pages/Admin/NewCompany'))
-const Singlecompany = lazy(() => import('./pages/Companies/Singlecompany'))
 const Companydashboard = lazy(() => import('./pages/Companies/Companydashboard'))
 const AllCompanyJobs = lazy(() => import('./pages/Companies/AllCompanyJobs'))
 const Allapplications = lazy(() => import('./pages/Companies/Allapplicationscompany'))
-const Resetpassword = lazy(() => import('./pages/User/Resetpassword'))
-const IsEmployee = lazy(() => import('./components/auth/IsEmployee'))
 const AllEmployees = lazy(() => import('./pages/Companies/AllEmployees'))
 const Editcompany = lazy(() => import('./pages/Admin/Editcompany'))
-const Isadmin = lazy(() => import('./components/auth/Isadmin'))
 const Assignusertocompanies = lazy(() => import('./pages/Admin/Assignusertocompanies'))
-const IsOwnerandloggedIn = lazy(() => import('./components/auth/isOwnerAndLoggedIn'))
 const Admindashbaoard = lazy(() => import('./pages/Admin/Admindashboard'))
-import Home from './pages/Home'
-const Jobs = lazy(() => import("./pages/Jobs/Alljob"))
 const EachJob = lazy(() => import('./pages/Jobs/eachJobs'))
-const Login = lazy(() => import('./pages/User/Login'))
-const Signup = lazy(() => import('./pages/User/Signup'))
-const VerifyEmail = lazy(() => import('./pages/User/Verifyemail'))
-const IsloggedinUser = lazy(() => import("./components/auth/isLoggedInUser"))
 const AllBookmarks = lazy(() => import('./pages/Jobs/AllBookmarks'))
 const EditJob = lazy(() => import('./pages/Jobs/EditJob'))
 const Newjob = lazy(() => import('./pages/Jobs/Newjob'))
-const Searchjobs = lazy(() => import('./pages/Jobs/Searchjobs'))
 
-
+import Singlecompany from './pages/Companies/Singlecompany'
+import Resetpassword from './pages/User/Resetpassword'
+import VerifyEmail from './pages/User/Verifyemail'
+import Jobs from "./pages/Jobs/Alljob"
+import IsOwnerandloggedIn from './components/auth/isOwnerAndLoggedIn'
+import IsEmployee from './components/auth/IsEmployee'
+import Isadmin from './components/auth/Isadmin'
+import Searchjobs from './pages/Jobs/Searchjobs'
+import IsloggedinUser from "./components/auth/isLoggedInUser"
+import AuthProvider from './context/Authcontext'
+import Home from './pages/Home'
+import Login from './pages/User/Login'
+import Signup from './pages/User/Signup'
+import ProfilePhoto from './pages/User/addProfilePhoto'
 import Errorpage from './pages/Errorpage'
 import Loading from './components/Loading'
 import CompanyFollowers from './pages/Companies/CompanyFollowers'
