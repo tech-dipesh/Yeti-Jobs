82f388e6622453557dc5a1d8b3aad1a62e185e5b
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-03-27 11:38:09 +0530

    Imporove the cron task with optimized way for change the job status

--- backend/src/services/cron-task.js
+++ backend/src/services/cron-task.js
@@ -1,29 +1,11 @@
 import cron from "node-cron";
 import connect from "../db.js";
 cron.schedule("0 0 * * *", async () => {
-  const { rows } = await connect.query("select created_at, uid from jobs");
-  const todayDate = new Date().toLocaleDateString();
-  const newMap = rows.map((row) => ({
-    created_at: row.created_at.toLocaleDateString(),
-    uid: row.uid,
-  }));
   try {
-    const checkWhichNeedUpdate = newMap.filter((n) => {
-      if (n.created_at < todayDate) {
-        return { n };
-      }
-    });
-    if (checkWhichNeedUpdate.length > 0) {
-      for (let i of checkWhichNeedUpdate) {
-        await connect.query(
-          "update jobs set is_job_open='closed' where uid=$1",
-          [i.uid],
-        );
-      }
-    }
-    return res.status(200).json({message: "data cleaned successfuly"});
+    const {rows} = await connect.query("update jobs set is_job_open = 'closed' where created_at < CURRENT_DATE and is_job_open = 'open'");
+    console.log(`Total ${rows.length} Data Cleaned Successfully.`);
   } catch (error) {
-    return res.status(501).json({ message: error.message });
+    console.log(error.message );
   }
 });
 
