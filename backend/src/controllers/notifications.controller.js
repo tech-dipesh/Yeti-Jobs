72f3da3e1abdd256fe695b2fac4e93bc3ff12767
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-06-17 09:16:21 +0545

    Update a Notification Controller it's not robust yet but will return a basic output to show on the client side

--- backend/src/controllers/notifications.controller.js
+++ backend/src/controllers/notifications.controller.js
@@ -1,29 +1,46 @@
 import client from '../db.js';
-export const showAllNotifications=(req, res)=>{
+
+import validateCorrectUid from '../utils/ValidateFunUid.js'
+export const showAllNotifications=async(req, res)=>{
+  const {uid}=req.user;
+  const {unread}=req.query
+  console.log('unread', unread)
+  console.log('uid', uid)
   try {
-    return res.status(200).json({ message:  "Notifications Show All notification"});
+    const {rows}=await client.query("select * from notifications where users_id=$1", [uid]);
+    return res.status(200).json({ message:  rows});
   } catch (error) {
     console.error(error);
    return res.status(500).json({ message: error.message }); 
   }
 }
-export const ReadAllNotifications=(req, res)=>{
-  const {id}=req.params
+export const ReadAllNotifications=async(req, res)=>{
   const {uid}=req.user;
   try {
-    return res.status(200).json({ message:  uid});
+    await client.query("update notifications set read_at=current_timestamp where uid=$1", [uid]);
+    return res.status(200).json({ message:  "Successfully Read All notifications"});
   } catch (error) {
     console.error(error);
    return res.status(500).json({ message: error.message }); 
   }
 }
 export const sendUserNotifications=async(req, res)=>{
-  const {id}=req.params
+  const {job_id, company_id, type}=req.body ?? {}
   const {uid}=req.user;
+  
+  const allTypes=["new_jobs","application_status","job_alert","bookmark_reminder","company_follow","application_recieved","profile_view","message_recieved","resume_analysed","announcement"]
   try {
-    const {rows}=await client.query("select * from notifications where users_id=$1", [uid])
-    console.log('rows is', rows);
-    return res.status(200).json({ message:  "Send User Notification Pos"});
+    if(validateCorrectUid(job_id) || validateCorrectUid((company_id))){
+      return res.status(400).json({message: "Please Enter a Valid UUID at Company and job Id"})
+    }
+    if(!job_id || !company_id || !type){
+      return res.status(422).json({message: "Please Enter a Job Id, Company Id and Type"})
+    }
+    if(!allTypes.includes((type))){
+     return res.status(400).json({message: "Please Only Enter a Allowed Types"}) 
+    }
+    const {rows}=await client.query("insert into notifications (users_id, job_id, company_id, type) values($1, $2, $3, $4) returning *;", [uid, job_id, company_id, type])
+    return res.status(200).json({ message:  rows});
   } catch (error) {
     console.error(error);
    return res.status(500).json({ message: error.message }); 
