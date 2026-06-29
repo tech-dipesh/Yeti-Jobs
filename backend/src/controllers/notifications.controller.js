c9a83b56550db16350165e80825245c435f73ab7
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-06-29 17:50:49 +0530

    Add one more routes of making a read or the unread any notifications to the any users, the send notifications still not corrected which i'll fix with using a bullmq of the background, with write a raw sql

--- backend/src/controllers/notifications.controller.js
+++ backend/src/controllers/notifications.controller.js
@@ -7,13 +7,37 @@ export const showAllNotifications=async(req, res)=>{
   console.log('unread', unread)
   console.log('users id', uid)
   try {
-    const {rows}=await client.query("select * from notifications where users_id=$1 order by created_at desc", [uid]);
+    const {rows}=await client.query("select n.uid, n.type, n.created_at, n.read_at, n.job_id, n.company_id, n.users_id, j.title as job_title, c.name as company_name from notifications n left join jobs j on j.uid = n.job_id left join companies c on c.uid = n.company_id WHERE n.users_id =$1 order by n.created_at desc", [uid]);
     return res.status(200).json({ message:  rows});
   } catch (error) {
     console.error(error);
    return res.status(500).json({ message: error.message }); 
   }
 }
+export const ReadUnreadSingleNotifications=async(req, res)=>{
+  const {uid:userId}=req.user;
+  const {uid}=req.params;
+  const {isRead}=req.query
+  try {
+    let checkCondition; 
+    if(isRead){
+      checkCondition==await client.query("update notifications set read_at=current_timestamp where uid=$1 and users_id=$2 and read_at is null", [uid, userId]);
+    }
+    else{
+      checkCondition=await client.query("update notifications set read_at=null where uid=$1 and users_id=$2 and read_at is not null", [uid, userId]);
+    }
+    const {rowCount}=checkCondition
+    if(!rowCount){
+      return res.status(404).json({message: "notifications is already read or No notifications exist"})
+    }
+   return res.status(200).json({ message:  "Successfully read/unread notifications"});
+  } catch (error) {
+    console.error(error);
+   return res.status(500).json({ message: error.message }); 
+  }
+}
+
+
 export const ReadAllNotifications=async(req, res)=>{
   const {uid}=req.user;
   try {
