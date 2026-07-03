b9de5d968db548a8d59ba84daaaa5807443fdcf5
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-07-03 10:58:31 +0530

    Add The How many time it've added of the Notitifications

---
+++ frontend/src/services/Timeago.js
@@ -0,0 +1,9 @@
+const Timeago = d => {
+  const s = Math.floor((Date.now() - new Date(d)) / 1000)
+  if (s < 60) return `${s}s ago`
+  if (s < 3600) return `${Math.floor(s/60)}m ago`
+  if (s < 86400) return `${Math.floor(s/3600)}h ago`
+  return `${Math.floor(s/86400)}d ago`
+}
+
+export default Timeago;
