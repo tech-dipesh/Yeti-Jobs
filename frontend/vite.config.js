dfa730fc534453e28bb3cb8219f95bb15c9e38c1
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-09-06 14:49:21 +0530

    Add the Alias Setup on the Project Setup which is a better optimized way to use it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import path from "node:path"
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from 'url';
 // https://vite.dev/config/
+// 1. Define __dirname manually for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
